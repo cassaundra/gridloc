b97f57eaad836daec822b2cc7301cbab30615473
Cassaundra <cass@cassaundra.io>
2020-11-10 16:15:06 -0800

    Implement eval tapes

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -6,25 +6,23 @@ use crate::*;
 mod parser;
 pub use parser::*;
 
-#[derive(Default)]
 pub struct Interpreter<'a, G: Grid> {
-    source: &'a str,
     reader: Option<BufReader<&'a mut dyn Read>>,
     writer: Option<BufWriter<&'a mut dyn Write>>,
     state: ProgramState<G>,
 }
 
 impl<'a, G: Grid> Interpreter<'a, G> {
+    pub fn state(&self) -> &ProgramState<G> {
+        &self.state
+    }
+
     pub fn run(&mut self) -> io::Result<()> {
-        unimplemented!()
+        Ok(())
     }
 
     pub fn step(&mut self) -> io::Result<()> {
-        unimplemented!()
-    }
-
-    pub fn state(&self) -> &ProgramState<G> {
-        &self.state
+        Ok(())
     }
 }
 
@@ -54,11 +52,54 @@ impl<'a> InterpreterBuilder<'a> {
     }
 
     pub fn build<G: Grid>(self) -> Interpreter<'a, G> {
+        let tape = SourceTape::from(self.source);
+        let program_state = ProgramState::new(Box::new(tape));
+
         Interpreter {
-            source: self.source,
             reader: self.reader,
             writer: self.writer,
-            ..Default::default()
+            state: program_state,
+        }
+    }
+}
+
+struct SourceTape {
+    source: Vec<u8>,
+    index: usize,
+}
+
+impl From<&[u8]> for SourceTape {
+    fn from(slice: &[u8]) -> SourceTape {
+        Self {
+            source: slice.to_owned(),
+            index: 0,
+        }
+    }
+}
+
+impl From<&str> for SourceTape {
+    fn from(s: &str) -> SourceTape {
+        s.as_bytes().into()
+    }
+}
+
+impl EvalTape for SourceTape {
+    fn next(&mut self) -> Option<u8> {
+        if self.index < self.source.len() {
+            let value = self.source[self.index];
+            self.index += 1;
+            Some(value)
+        } else {
+            None
+        }
+    }
+
+    fn prev(&mut self) -> Option<u8> {
+        if self.index > 0 {
+            self.index -= 1;
+            Some(self.source[self.index])
+        } else {
+            None
         }
     }
 }
--- src/program/mod.rs
+++ src/program/mod.rs
@@ -1,30 +1,145 @@
+use std::cell::RefCell;
 use std::collections::BTreeMap;
-
-use std::ops::{Add, Sub};
+use std::ops::{Add, Sub, Mul};
+use std::rc::Rc;
 
 mod grid;
 pub use grid::*;
 
-#[derive(Clone, Debug)]
 pub struct ProgramState<G: Grid> {
-    pub grid: G,
-    pub pointers: Vec<Pointer>,
-    pub saved_positions: BTreeMap<u8, Position>,
-    pub string_mode: Option<StringModeKind>,
+    eval_tapes: Vec<Box<dyn EvalTape>>,
+    grid: Rc<G>,
+    pointers: Vec<Rc<RefCell<Pointer>>>,
+    saved_positions: BTreeMap<u8, Position>,
+    string_mode: Option<StringModeKind>,
 }
 
-impl<G: Grid> Default for ProgramState<G> {
-    fn default() -> Self {
+impl<G: Grid> ProgramState<G> {
+    pub fn new(program: Box<dyn EvalTape>) -> Self {
         Self {
-            pointers: vec![Pointer::default()],
-            ..Default::default()
+            eval_tapes: vec![program],
+            grid: Rc::new(G::default()),
+            pointers: vec![Rc::new(RefCell::new(Pointer::default()))],
+            saved_positions: BTreeMap::new(),
+            string_mode: None,
         }
+    }
+
+    pub fn step(&mut self) {
+        
+    }
+
+    fn execute_instruction(&mut self, instruction: Instruction) {
+        use Instruction::*;
+
+        match instruction {
+            SetDirection(direction) => {
+
+            }
+            Move => {
+            }
+            ReadValue => {
+            }
+            WriteValue => {
+            }
+            SwapValue => {
+            }
+            StartLoop => {
+            }
+            EndLoop => {
+            }
+            PushPosition => {
+            }
+            PopPosition => {
+            }
+            SwapPosition => {
+            }
+            SavePosition => {
+            }
+            LoadPosition => {
+            }
+            GoToColumn => {
+            }
+            GoToRow => {
+            }
+            ToggleStringMode { kind: StringModeKind } => {
+            }
+            Evaluate => {
+            }
+            Kill => {
+            }
+            Value(value) => {
+            }
+            Add => {
+            }
+            Subtract => {
+            }
+            Multiply => {
+            }
+            Divide => {
+            }
+            Modulo => {
+            }
+            Equals => {
+            }
+            GreaterThan => {
+            }
+            LogicalNot => {
+            }
+            ReadInput => {
+            }
+            PrintCharacter { new_line: bool } => {
+            }
+            PrintNumber { new_line: bool } => {
+            }
+        }
+    }
+}
+
+pub trait EvalTape {
+    fn next(&mut self) -> Option<u8>;
+    fn prev(&mut self) -> Option<u8>;
+}
+
+struct GridTape<G: Grid> {
+    pointer: Rc<RefCell<Pointer>>,
+    grid: Rc<G>,
+}
+
+impl<G: Grid> EvalTape for GridTape<G> {
+    fn next(&mut self) -> Option<u8> {
+        let pointer = &mut self.pointer.borrow_mut();
+        let value = self.grid.get(&pointer.position);
+
+        if value != 0 {
+            // move the pointer forward
+            let delta = pointer.direction.unit_vector();
+            pointer.position = pointer.position + delta;
+
+            Some(value)
+        } else {
+            None
+        }
+    }
+
+    fn prev(&mut self) -> Option<u8> {
+        let pointer = &mut self.pointer.borrow_mut();
+
+        // move the pointer backwards
+        let delta = pointer.direction
+            .opposite()
+            .unit_vector();
+        pointer.position = pointer.position + delta;
+
+        let value = self.grid.get(&pointer.position);
+        Some(value)
     }
 }
 
 #[derive(Clone, Debug, Default)]
 pub struct Pointer {
     pub position: Position,
+    pub direction: Direction,
     pub stack: Vec<Position>,
     pub value: u8,
 }
@@ -57,11 +172,19 @@ impl Add for Position {
 impl Sub for Position {
     type Output = Self;
 
-    fn sub(self, other: Self) -> Self {
+    fn sub(self, other: Self) -> Self::Output {
         Self {
             x: self.x.wrapping_sub(other.x),
             y: self.y.wrapping_sub(other.y),
         }
+    }
+}
+
+impl Mul<isize> for Position {
+    type Output = Position;
+
+    fn mul(self, rhs: isize) -> Self::Output {
+        unimplemented!()
     }
 }
 
@@ -100,10 +223,40 @@ pub enum Instruction {
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Direction {
-    Left,
     Right,
+    Left,
     Up,
     Down,
+}
+
+impl Default for Direction {
+    fn default() -> Self {
+        Direction::Right
+    }
+}
+
+impl Direction {
+    pub fn opposite(&self) -> Direction {
+        use Direction::*;
+
+        match self {
+            Right => Left,
+            Left => Right,
+            Up => Down,
+            Down => Up,
+        }
+    }
+
+    pub fn unit_vector(&self) -> Position {
+        use Direction::*;
+
+        match self {
+            Right => Position::new(1, 0),
+            Left => Position::new(-1, 0),
+            Up => Position::new(0, 1),
+            Down => Position::new(0, -1),
+        }
+    }
 }
 
 #[derive(Copy, Clone, Debug, PartialEq)]
